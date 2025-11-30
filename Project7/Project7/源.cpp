d4f13886874e4e89e9e71448d7773280ae306708
f-atpig <abcd787894321@163.com>
2025-11-30 13:59:22 +0800

    study day 8

--- Project7/Project7/源.cpp
+++ Project7/Project7/源.cpp
@@ -3,9 +3,51 @@
 
 int main()
 {
+	//int a, b;
+	//scanf("%d%d", &a, &b);
+	//printf("%d %d", (a / b),(a%b) );
+	/*int num1;
 
+	printf("请输入一个整数：\n");
+	scanf("%d", &num1);
 
+	if (num1 % 2 != 0)
+		printf("%d是奇数\n", num1);
+	else
+		printf("%d是偶数\n", num1);*/
 
+	//int n = 0;
+	//int num = 0;
+	//while (num < 101)
+	//{
+	//	if (num % 2 != 0)
+	//	{
+	//		printf("%d\t", num);
+	//		++n;
+	//	}
+	//	++num; 
+	//	if (0 == n % 5) printf("\n");
+	//}
+	int day = 0;
+	scanf("%d", &day);
+	const int n = 1;
 
+	switch (day)
+	{
+	case n:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("weekday\n");
+		break;
+	case 6:
+	case 7:
+		printf("weekend\n");
+		break;
+	default:
+		printf("选择错误\n");
+		break;
+	}
 	return 0;
 }
