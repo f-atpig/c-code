b7fccd2d851a3a954186ba50a9b9deda22debfcb
f-atpig <abcd787894321@163.com>
2025-12-09 11:10:41 +0800

    study day 10

---
+++ Project9/Project9/源.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int MAX(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else return y;
+//};
+int main()
+{
+	//int arr[] = { 73,32,99,97,110,32,100,111,32,105,116 ,33 };
+	//int n = 0;
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//while (n<sz)
+	//{
+	//	printf("%c", arr[n]);
+	//	n++;
+	//}
+	//printf("请输入你的出生日期(例19491001):");
+	//int born = 0;
+	//int num = 0;
+	//while ((born = getchar())!='\n')
+	//{
+	//	num++;
+	//	if (num == 1) printf("year=");
+	//	if (num == 5) printf("\nmonth=");
+	//	if (num == 7) printf("\ndate=");
+	//	putchar(born);
+	//}
+	//int year, month, date;
+	//scanf("%4d%2d%2d", &year, &month, &date);
+	//printf("year=%d\n", year);
+	//printf("month=%02d\n", month);
+	//printf("year=%02d\n", date);
+	//int a = printf("hello world!");
+	//printf("\n%d\n", a);
+	//int arr[4] = {0};
+	//int i1 = 0;
+	//printf("请输入四个数进行比较:");
+	//while (i1 < 4)
+	//{
+	//	scanf("%d", &arr[i1]);
+	//	i1++;
+	//}
+	//int r = 1;
+	//int max = arr[0];
+	//while (r < 4)
+	//{
+	//	if (arr[r] > max)
+	//		max = arr[r];
+	//	r++;
+	//}
+	//printf("%d\n", max);
+
+
+
+
+
+	return 0;
+}
